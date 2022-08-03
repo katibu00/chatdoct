d05494a52f73d3d9570dbecbe84e30723a6d70f9
katibu00 <ukmisau@gmail.com>
2022-08-03 07:28:00 +0100

    chatbot

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -6,6 +6,20 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="author" content="chatdoc" />
 
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/62ea147c37898912e960f6b8/1g9h602rn';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+
     <!-- Stylesheets
  ============================================= -->
     <link
